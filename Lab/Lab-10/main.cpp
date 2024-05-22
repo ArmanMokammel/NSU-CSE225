25b8153914eb86cb87ac458ee2f08811a6cda80f
Arman Mokammel <armanmokammel@gmail.com>
2024-05-22 18:23:34 +0600

    Added comments

--- Lab/Lab-10/main.cpp
+++ Lab/Lab-10/main.cpp
@@ -4,6 +4,7 @@
 
 using namespace std;
 
+/* Function to clear out all spaces */
 string trimStr(string str)
 {
     int x = 0;
@@ -22,6 +23,7 @@ string trimStr(string str)
     return result;
 }
 
+/* Function to check the precedence of each operator */
 int checkPrecedence(char c)
 {
     switch(c)
@@ -37,6 +39,7 @@ int checkPrecedence(char c)
     }
 }
 
+/* You know what dis is :) */
 void postFixTOEval(string inputExpression)
 {
     StackType<char> stack;
@@ -44,54 +47,70 @@ void postFixTOEval(string inputExpression)
     string expression = "";
     string result;
 
+    //Get the space trimmed expression
     expression = trimStr(inputExpression);
     
+    //For each character in the expression string
     for(int i = 0; i < expression.length(); i++)
     {
         char c = expression[i];
 
+        //This is to check if there are two operators side by side
+        //If yes then invalid. Precedence function returns -1 if not operator
         if(checkPrecedence(expression[i]) != -1 && checkPrecedence(expression[i+1]) != -1)
         {
             cout << "Invalid expression" << endl << endl;
             return;
         }
 
+        //If the character is a digit
         if((c >= '0' && c <= '9'))
         {
+            //Put it directly to result
             result += c;
         }
         else if(c == '(')
         {
+            //Push it to stack
             stack.Push(c);
         }
         else if(c == ')')
         {
             try
             {
+                //Put all the operators of the stack directly to result until it reaches "("
                 while (stack.Top() != '(') {
                     result += stack.Top();
                     stack.Pop();
                 }
+                //Then also discard the "(" from stack
                 stack.Pop();
             }
             catch (EmptyStack e)
             {
+                //If it fails.. this means stack is empty. So must be invalid expression
                 cout << "Invalid Expression" << endl << endl;
                 return;
             }
         }
         else
         {
+            //If it is a operator
+            //Check the precedence.. It must be less than or equal to the stack's top element
             while (!stack.IsEmpty() && (checkPrecedence(expression[i]) < checkPrecedence(stack.Top()) || checkPrecedence(expression[i]) == checkPrecedence(stack.Top()))) 
             {
+                //Put the top elements to result
                 result += stack.Top();
                 stack.Pop();
             }
+            //Push the new operand to stack
             stack.Push(c);
             result += ' ';
         }
     }
 
+    //Empty out the stack to the result, if there is anything.
+    //If a blank "(" or ")" is found then the expression was invalid
     while (!stack.IsEmpty()) {
         if(stack.Top() == '(' || stack.Top() == ')')
         {
@@ -102,6 +121,7 @@ void postFixTOEval(string inputExpression)
         stack.Pop();
     }
 
+    //Print the post fix form
     cout << result << endl;
 
     expression = result;
