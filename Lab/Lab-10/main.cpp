be4f40383dba668d20ca377c05be0c11407cb5fc
Arman Mokammel <armanmokammel@gmail.com>
2024-05-22 18:09:23 +0600

    hw complete

--- Lab/Lab-10/main.cpp
+++ Lab/Lab-10/main.cpp
@@ -4,6 +4,24 @@
 
 using namespace std;
 
+string trimStr(string str)
+{
+    int x = 0;
+    string result;
+
+    while(str[x] != '\0')
+    {
+        if(str[x] == ' ')
+        {
+            x++;
+            continue;
+        }
+        result += str[x];
+        x++;
+    }
+    return result;
+}
+
 int checkPrecedence(char c)
 {
     switch(c)
@@ -19,35 +37,26 @@ int checkPrecedence(char c)
     }
 }
 
-int main()
+void postFixTOEval(string inputExpression)
 {
     StackType<char> stack;
 
-    string inputExpression;
     string expression = "";
     string result;
 
-    cout << "Enter expression: ";
-    getline(cin, inputExpression);
+    expression = trimStr(inputExpression);
     
-    int x = 0;
-    while(inputExpression[x] != '\0')
-    {
-        if(inputExpression[x] == ' ')
-        {
-            x++;
-            continue;
-        }
-
-        expression += inputExpression[x];
-        x++;
-    }
-
     for(int i = 0; i < expression.length(); i++)
     {
         char c = expression[i];
 
-        if((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9'))
+        if(checkPrecedence(expression[i]) != -1 && checkPrecedence(expression[i+1]) != -1)
+        {
+            cout << "Invalid expression" << endl << endl;
+            return;
+        }
+
+        if((c >= '0' && c <= '9'))
         {
             result += c;
         }
@@ -57,11 +66,19 @@ int main()
         }
         else if(c == ')')
         {
-            while (stack.Top() != '(') {
-                result += stack.Top();
+            try
+            {
+                while (stack.Top() != '(') {
+                    result += stack.Top();
+                    stack.Pop();
+                }
                 stack.Pop();
             }
-            stack.Pop();
+            catch (EmptyStack e)
+            {
+                cout << "Invalid Expression" << endl << endl;
+                return;
+            }
         }
         else
         {
@@ -76,11 +93,85 @@ int main()
     }
 
     while (!stack.IsEmpty()) {
+        if(stack.Top() == '(' || stack.Top() == ')')
+        {
+            cout << "Invalid Expression" << endl << endl;;
+            return;
+        }
         result += stack.Top();
         stack.Pop();
     }
- 
+
     cout << result << endl;
+
+    expression = result;
+    
+    StackType<double> evalStack;
+    double temp = 0;
+    for(int i = 0; i < expression.length(); i++)
+    {
+        char c = expression[i];
+
+        if(c == ' ')
+        {
+            if(temp == 0)
+                continue;
+
+            evalStack.Push(temp);
+            temp = 0;
+        }
+        else if((c >= '0' && c <= '9'))
+        {
+            temp = (10 * temp) + (c - '0');
+        }
+        else
+        {
+            if (temp != 0)
+            {
+                evalStack.Push(temp);
+                temp = 0;
+            }
+            double a,b, val = 0;
+            
+            b = evalStack.Top();
+            evalStack.Pop();
+            a = evalStack.Top();
+            evalStack.Pop();
+
+            switch(c)
+            {
+                case '+':
+                    val = a + b;
+                    break;
+                case '-':
+                    val = a - b;
+                    break;
+                case '*':
+                    val = a * b;
+                    break;
+                case '/':
+                    val = a / b;
+                    break;
+            }
+
+            evalStack.Push(val);
+        }
+    }
+
+    cout << "result = " << evalStack.Top() << endl << endl;
+}
+
+int main()
+{
+    while(1)
+    {
+        string inputExpression;
+
+        cout << "Enter expression: ";
+        getline(cin, inputExpression);
+
+        postFixTOEval(inputExpression);
+    }
 
     return 0;
 }
